f0bd4df7ece85484a5c1208920393498501d223d
alejandroforcada <alejandroforcadacs@gmail.com>
2023-11-08 09:54:52 +0100

    cambio de images team a base de datos en vez de fija, terminado

--- Plantilla2_symfony/src/Controller/PageController.php
+++ Plantilla2_symfony/src/Controller/PageController.php
@@ -14,6 +14,9 @@ use App\Form\joinUsFormType;
 use App\Form\RegistrationFormType;
 use App\Security\AppCustomAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
@@ -21,11 +24,13 @@ use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 class PageController extends AbstractController
 {
     #[Route('/', name: 'inicio')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine ):Response
     {
-        return $this->render('page/inicio.html.twig', [
-            'controller_name' => 'PageController',
-        ]);
+        $repositorio=$doctrine->getRepository(Trabajador::class);
+            $trabajadores=$repositorio->findAll();
+            return $this -> render ('page/inicio.html.twig', [
+                'trabajadores' => $trabajadores
+            ]);
     }
     #[Route('/about', name: 'about')]
     public function about(): Response
@@ -42,11 +47,13 @@ class PageController extends AbstractController
         ]);
     }
     #[Route('/team', name: 'team')]
-    public function team(): Response
-    {
-        return $this->render('page/team.html.twig', [
-            'controller_name' => 'PageController',
-        ]);
+        public function team(ManagerRegistry $doctrine ):Response
+        {
+            $repositorio=$doctrine->getRepository(Trabajador::class);
+                $trabajadores=$repositorio->findAll();
+                return $this -> render ('page/team.html.twig', [
+                    'trabajadores' => $trabajadores
+                ]);
     }
     #[Route('/why', name: 'why')]
     public function why(): Response
@@ -56,21 +63,45 @@ class PageController extends AbstractController
         ]);
     }
     #[Route('/join_us', name: 'join_us')]
-    public function joinUs(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppCustomAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function joinUs(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppCustomAuthenticator $authenticator, EntityManagerInterface $entityManager, SessionInterface $session, SluggerInterface $slugger): Response
     {
         $trabajador = new Trabajador();
-        $form = $this->createForm(joinUsFormType::class, $trabajador);
-        $form->handleRequest($request);
+        $formulario = $this->createForm(joinUsFormType::class, $trabajador);
+        $formulario->handleRequest($request);
+        
 
-      
+        if($formulario->isSubmitted()&& $formulario->isValid()){
+                $trabajador=$formulario->getData();
+                $file = $formulario->get('foto')->getData();
+                if ($file) {
+                    $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+                    // Move the file to the directory where images are stored
+                    try {
+
+                        $file->move(
+                            $this->getParameter('images_directory'), $newFilename
+                        );                      
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                        return new Response($e->getMessage());
+                    }
+
+                    // updates the 'file$filename' property to store the PDF file name
+                    // instead of its contents
+                    $trabajador->setFoto($newFilename);
+                }
 
             $entityManager->persist($trabajador);
             $entityManager->flush();
             // do anything else you need here, like send an email
-
+            }
 
         return $this->render('page/joinUs.html.twig', [
-            'formulario' => $form->createView(),
+            'formulario' => $formulario->createView(),
         ]);
     }
 }
--- Plantilla2_symfony/src/Form/joinUsFormType.php
+++ Plantilla2_symfony/src/Form/joinUsFormType.php
@@ -13,6 +13,14 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
 class joinUsFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -20,6 +28,19 @@ class joinUsFormType extends AbstractType
         $builder
             ->add('nombre')
             ->add('puesto')
+            ->add('foto', FileType::class,[
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image file',
+                    ])
+                ],
+            ])
+            ->add('save', SubmitType::class, array('label' => 'Enviar'));
        
         ;
     }
